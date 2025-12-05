f45d4d8f572989f9b04d4ec80e5c0466a643eff5
Ola Fjelddahl <ola.fjelddahl@omegapoint.se>
2025-12-05 22:47:33 +0100

    feat:Allow showing the result as text and foldable - weird update for making keys visible.

--- src/components/ResultsView.tsx
+++ src/components/ResultsView.tsx
@@ -85,7 +85,24 @@ export const ResultsView: React.FC<ResultsViewProps> = ({ results, loading }) =>
             <div className="json-viewer-container">
               <ReactJson
                 src={results}
-                theme="rjv-default"
+                theme={{
+                  base00: "transparent", // background
+                  base01: "#383830",
+                  base02: "#49483e",
+                  base03: "#75715e",
+                  base04: "#a59f85",
+                  base05: "#f8f8f2", // text
+                  base06: "#f5f4f1",
+                  base07: "#f9f8f5",
+                  base08: "#f92672",
+                  base09: "#fd971f",
+                  base0A: "#f4bf75",
+                  base0B: "#a6e22e", // string
+                  base0C: "#a1efe4",
+                  base0D: "#66d9ef", // key
+                  base0E: "#ae81ff",
+                  base0F: "#cc6633"
+                }}
                 displayDataTypes={false}
                 style={{ backgroundColor: 'transparent' }}
                 collapsed={1}
